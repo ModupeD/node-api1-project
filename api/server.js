24c58aa0aed024367728ad5835e1e505a8f15184
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-01 11:32:29 +0100

    more changes

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ server.get("/api/users", (req, res) => {
   })
   .catch(err => {
       res.status(500).json({
-          message: 'error getting users',
+          message: 'error getting user',
           err: err.message,
           stack: err.stack,
       })
